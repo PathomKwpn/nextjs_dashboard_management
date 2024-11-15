80818584f409375fcfd684211d3a91cbb74499dc
Pathompong <Pathompong.forwork@gmail.com>
2024-11-15 16:35:04 +0700

    [#FRONTEND] add list folder / edit formModal&BigCalendar

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -8,6 +8,16 @@ import Image from "next/image";
 const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
   loading: () => <h1>Loading...</h1>,
 });
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+  teacher: (type, data) => <TeacherForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />,
+};
 const FormModal = ({
   table,
   type,
